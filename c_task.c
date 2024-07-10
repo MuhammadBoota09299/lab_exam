50ddb422ddc99ba3260f49f5e0de6565b37a1a6e
MuhammadBoota09299 <125748230+MuhammadBoota09299@users.noreply.github.com>
2024-07-10 17:18:56 +0500

    Add files via upload

--- c_task.c
+++ c_task.c
@@ -3,24 +3,22 @@ int main(){
     int Q=8;//dividend
     int M=2;//divisor
     int A=0;
-    int x=A;
+    int x;
     int n=sizeof(Q);//number of bit of dividend
-    if (sizeof(A)==1 && sizeof(Q)==1){ //if A and Q are single unit
-        A<<1;//shifted left
-        Q<<1;//shifted left
-        A=A-M;
+    if (sizeof(A)!=1 && sizeof(Q)!=1){ //if A and Q are single unit
         while (n!=0){ //while n!=0 repeat below steps
-        if (A==0){ //if most significant bit of A is zero
-            Q[0]=1;//put least significant bit of q is 1
+        A=A<<1;//shifted left
+        x=A;
+        Q=Q<<1;//shifted left
+        A=A-M;
+        if (!(A<<(sizeof(A)-1)) & 1){ //if most significant bit of A is zero
+            Q=Q|1;//put least significant bit of q is 1
         }else{ 
-           Q[0]=0;//put least significant bit of q is 0
+           Q=Q|0;//put least significant bit of q is 0
             A=x;// restore A 
         }
         n=n-1;
         }
     }
-    else{
         printf("quotient =%d\nremainder =%d\n",Q,A);
     }
-    
-}
